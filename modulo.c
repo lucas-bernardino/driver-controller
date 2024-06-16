714ec6573de6af05886b8c8f043fcd3b2d1e4082
lucas-bernardino <lucasgber@gmail.com>
2024-06-16 16:14:57 +0000

    Format code

--- modulo.c
+++ modulo.c
@@ -6,8 +6,8 @@
 #include <linux/hid.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Lucas Nesi <email@email.com>");
-MODULE_DESCRIPTION("Meu Modulo USB");
+MODULE_AUTHOR("Lucas Bernardino, Gustavo Moro, Pedro Augusto Dantas Vargas");
+MODULE_DESCRIPTION("Xbox Series S Controller Driver");
 
 #define BUTTON_PRESSED 0x2C
 
@@ -57,15 +57,6 @@ static void read_callback(struct urb *urb) {
   struct usb_controller *controller = urb->context;
   unsigned char* data = controller->buffer;
 
-  // printk(KERN_INFO "data[0]=%X\n", data[0]);
-  // printk(KERN_INFO "data[1]=%X\n", data[1]);
-  // printk(KERN_INFO "data[2]=%X\n", data[2]);
-  // printk(KERN_INFO "data[3]=%X\n", data[3]);
-  // printk(KERN_INFO "data[4]=%X\n", data[4]);
-  // printk(KERN_INFO "data[5]=%X\n", data[5]);
-  // printk(KERN_INFO "data[6]=%X\n", data[6]);
-  // printk(KERN_INFO "data[7]=%X\n\n", data[7]);
-  //
   handle_button_pressed(data, controller);
 
   if (!controller->i_dev) {
@@ -78,147 +69,146 @@ static void read_callback(struct urb *urb) {
 
 static int usb_controller_open(struct input_dev *i_dev) {
 
-   struct usb_controller *controller = input_get_drvdata(i_dev);
+  struct usb_controller *controller = input_get_drvdata(i_dev);
 
-   if (!controller) {
-	printk(KERN_WARNING "ERROR: Could not input_get_drvdata\n");
-	return 1;
-   }
+  if (!controller) {
+    printk(KERN_WARNING "ERROR: Could not input_get_drvdata\n");
+    return 1;
+  }
 
-   printk(KERN_ALERT "OPENING USB_CONTROLLER\n");
+  printk(KERN_ALERT "OPENING USB_CONTROLLER\n");
 
-   if (!controller->my_urb) {
-	printk(KERN_WARNING "Opps.. controller->my_urb is NULL\n");
-	return 1;
-   }
+  if (!controller->my_urb) {
+    printk(KERN_WARNING "Opps.. controller->my_urb is NULL\n");
+    return 1;
+  }
 
-   if (!controller->usb_dev) {
-	printk(KERN_WARNING "Opps.. controller->usb_dev is NULL\n");
-	return 1;
-   }
+  if (!controller->usb_dev) {
+    printk(KERN_WARNING "Opps.. controller->usb_dev is NULL\n");
+    return 1;
+  }
 
-   controller->my_urb->dev = controller->usb_dev;
+  controller->my_urb->dev = controller->usb_dev;
 
-   int status = usb_submit_urb(controller->my_urb, GFP_KERNEL);
+  int status = usb_submit_urb(controller->my_urb, GFP_KERNEL);
 
-   if (status) {
-	printk(KERN_WARNING "ERROR: Could not usb_submit_urb\n");
-	return 1;
-   }
+  if (status) {
+    printk(KERN_WARNING "ERROR: Could not usb_submit_urb\n");
+    return 1;
+  }
 
-   return 0;
+  return 0;
 
 }
 
 static void usb_controller_close(struct input_dev *i_dev) {
 
-   struct usb_controller *controller = input_get_drvdata(i_dev);
+  struct usb_controller *controller = input_get_drvdata(i_dev);
 
-   if (!controller) {
-	printk(KERN_WARNING "ERROR: Could not input_get_drvdata\n");
-	return;
-   }
+  if (!controller) {
+    printk(KERN_WARNING "ERROR: Could not input_get_drvdata\n");
+    return;
+  }
 
-   if (!controller->my_urb) {
-	printk(KERN_WARNING "Opps.. controller->my_urb is NULL\n");
-	return;
-   }
+  if (!controller->my_urb) {
+    printk(KERN_WARNING "Opps.. controller->my_urb is NULL\n");
+    return;
+  }
 
-   printk(KERN_ALERT "CLOSING USB_CONTROLLER\n");
+  printk(KERN_ALERT "CLOSING USB_CONTROLLER\n");
 
-   usb_kill_urb(controller->my_urb);
+  usb_kill_urb(controller->my_urb);
 
 }
 
 // USB - Probe - Função de entrada quando um novo dispositivo é reconhecido para este modulo
 static int meu_driver_usb_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-    int retval = 0;
+  int retval = 0;
 
-    struct usb_device *dev = interface_to_usbdev(interface);
+  struct usb_device *dev = interface_to_usbdev(interface);
 
-    if (interface->cur_altsetting->desc.bNumEndpoints != 2)
-		return -ENODEV;
+  if (interface->cur_altsetting->desc.bNumEndpoints != 2)
+    return -ENODEV;
 
-    printk(KERN_INFO "meu_driver_usb: O dispositivo idVendor=%X idProduct=%X foi connectado ao meu driver, interface=%X", id->idVendor, id->idProduct, interface->cur_altsetting->desc.bInterfaceNumber);
+  printk(KERN_INFO "meu_driver_usb: O dispositivo idVendor=%X idProduct=%X foi connectado ao meu driver, interface=%X", id->idVendor, id->idProduct, interface->cur_altsetting->desc.bInterfaceNumber);
 
-    int numendpoints = interface->cur_altsetting->desc.bNumEndpoints;
-	
-    printk(KERN_INFO "meu_driver_usb: interface=%X numEndpoints=%X", interface->cur_altsetting->desc.bInterfaceNumber, numendpoints);
+  int numendpoints = interface->cur_altsetting->desc.bNumEndpoints;
 
-    struct usb_controller *controller = kzalloc(sizeof(struct usb_controller) , GFP_KERNEL);
-    if (!controller) {
-      printk(KERN_WARNING "ERROR: Could not alloc controller.\n");
-      return retval;
-    }
+  printk(KERN_INFO "meu_driver_usb: interface=%X numEndpoints=%X", interface->cur_altsetting->desc.bInterfaceNumber, numendpoints);
 
-    controller->my_urb = usb_alloc_urb(0, GFP_KERNEL);
-    if (!controller->my_urb) {
-      printk(KERN_WARNING "ERROR: Could not alloc urb.\n");
-      return retval;
-    }
-
-    controller->name = "Xbox Series S Controller";
-    controller->usb_dev = dev;
-    
-    struct usb_endpoint_descriptor *ep_irq_in;
-    ep_irq_in = NULL;
-
-    for (int i = 0; i < 2; i++) {
-      struct usb_endpoint_descriptor *ep = &interface->cur_altsetting->endpoint[i].desc;
-      if (usb_endpoint_xfer_int(ep)) {
-        if (usb_endpoint_dir_in(ep))
-          ep_irq_in = ep;
-      }
-    }
-
-    if (!ep_irq_in) {
-      printk(KERN_WARNING "ERROR: Could not find interruption and in direction\n");
-      return retval;
-    }
-
-    struct input_dev *i_dev = input_allocate_device();
-    if(!i_dev) {
-      printk(KERN_WARNING "ERROR: Could not input_allocate_device\n");
-      return retval;
-    }
-    controller->i_dev = i_dev;
-
-    usb_to_input_id(controller->usb_dev, &i_dev->id);
-    i_dev->dev.parent = &interface->dev;
-    controller->i_dev->name = controller->name;
-    input_set_drvdata(i_dev, controller);
-
-    i_dev->open = usb_controller_open;
-    i_dev->close = usb_controller_close;
-
-
-    controller->pipe = usb_rcvintpipe(dev, ep_irq_in->bEndpointAddress);
-    controller->buffer = usb_alloc_coherent(dev, 64, GFP_KERNEL, &controller->input_dma_addr);
-    usb_fill_int_urb(controller->my_urb, dev, controller->pipe, controller->buffer, 64, read_callback, controller, ep_irq_in->bEndpointAddress);
- 
-
-    input_set_capability(controller->i_dev, EV_KEY, KEY_A);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_B);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_X);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_Y);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_N);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_M);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_U);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_D);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_L);
-    input_set_capability(controller->i_dev, EV_KEY, KEY_R);
-
-    int err = input_register_device(controller->i_dev);
-    printk(KERN_INFO "Value from err: %d\n", err);
-    if (err) {
-      printk(KERN_WARNING "ERROR: Could not input_register_device\n");
-      return 1;
-    }
-
-    usb_set_intfdata(interface, controller);
-
+  struct usb_controller *controller = kzalloc(sizeof(struct usb_controller) , GFP_KERNEL);
+  if (!controller) {
+    printk(KERN_WARNING "ERROR: Could not alloc controller.\n");
     return retval;
+  }
+
+  controller->my_urb = usb_alloc_urb(0, GFP_KERNEL);
+  if (!controller->my_urb) {
+    printk(KERN_WARNING "ERROR: Could not alloc urb.\n");
+    return retval;
+  }
+
+  controller->name = "Xbox Series S Controller";
+  controller->usb_dev = dev;
+
+  struct usb_endpoint_descriptor *ep_irq_in;
+  ep_irq_in = NULL;
+
+  for (int i = 0; i < 2; i++) {
+    struct usb_endpoint_descriptor *ep = &interface->cur_altsetting->endpoint[i].desc;
+    if (usb_endpoint_xfer_int(ep)) {
+      if (usb_endpoint_dir_in(ep))
+        ep_irq_in = ep;
+    }
+  }
+
+  if (!ep_irq_in) {
+    printk(KERN_WARNING "ERROR: Could not find interruption and in direction\n");
+    return retval;
+  }
+
+  struct input_dev *i_dev = input_allocate_device();
+  if(!i_dev) {
+    printk(KERN_WARNING "ERROR: Could not input_allocate_device\n");
+    return retval;
+  }
+  controller->i_dev = i_dev;
+
+  usb_to_input_id(controller->usb_dev, &i_dev->id);
+  i_dev->dev.parent = &interface->dev;
+  controller->i_dev->name = controller->name;
+  input_set_drvdata(i_dev, controller);
+
+  i_dev->open = usb_controller_open;
+  i_dev->close = usb_controller_close;
+
+
+  controller->pipe = usb_rcvintpipe(dev, ep_irq_in->bEndpointAddress);
+  controller->buffer = usb_alloc_coherent(dev, 64, GFP_KERNEL, &controller->input_dma_addr);
+  usb_fill_int_urb(controller->my_urb, dev, controller->pipe, controller->buffer, 64, read_callback, controller, ep_irq_in->bEndpointAddress);
+
+
+  input_set_capability(controller->i_dev, EV_KEY, KEY_A);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_B);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_X);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_Y);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_N);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_M);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_U);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_D);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_L);
+  input_set_capability(controller->i_dev, EV_KEY, KEY_R);
+
+  int err = input_register_device(controller->i_dev);
+  if (err) {
+    printk(KERN_WARNING "ERROR: Could not input_register_device\n");
+    return 1;
+  }
+
+  usb_set_intfdata(interface, controller);
+
+  return retval;
 }
 
 // USB - Disconnect - Função de saída quando um dispositivo é desconectado
